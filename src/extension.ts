fd79fd28be00abfca597a84cba9f428014d9404f
jtmhuka33 <38496930+jtmhuka33@users.noreply.github.com>
2024-04-24 11:48:21 +0200

    chore: wip (init success, now attempting file generation and error handling)

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,33 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+
+const fs = require("fs");
+const path = require("path");
+
+/**
+ * this function is triggered to generate files and code that resembles
+ * the schema of a typical blogging application
+ */
+function generateSchemaBlogging () {
+	const filepath = vscode.workspace.workspaceFolders + '/schema.sql';
+	const content = '-- SQL Database Preset One Schema\nCREATE TABLE example (id INT, data VARCHAR(100));';
+	fs.writeFileSync(filepath, content);
+	vscode.window.showInformationMessage('Blogging Schema Created');
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "datai" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('datai.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from datai!');
 	});
 
+	//Test
+	let bloggingSchema = vscode.commands.registerCommand('datai.bloggingApplicationSchema', generateSchemaBlogging);
+	context.subscriptions.push(bloggingSchema);
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
