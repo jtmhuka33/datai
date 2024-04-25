ca1357b01c7201dcee1977c2d835070c6b8694d6
jtmhuka33 <38496930+jtmhuka33@users.noreply.github.com>
2024-04-25 13:09:20 +0200

    feat: functional file creation and code generation, no api included yet

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,55 @@
+import * as vscode from "vscode";
 
-import * as vscode from 'vscode';
-
-
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
 
 /**
  * this function is triggered to generate files and code that resembles
  * the schema of a typical blogging application
  */
-function generateSchemaBlogging () {
-	const filepath = vscode.workspace.workspaceFolders + '/schema.sql';
-	const content = '-- SQL Database Preset One Schema\nCREATE TABLE example (id INT, data VARCHAR(100));';
-	fs.writeFileSync(filepath, content);
-	vscode.window.showInformationMessage('Blogging Schema Created');
+function generateSchemaBlogging() {
+  const workspaceFolders = vscode.workspace.workspaceFolders;
+  if (!workspaceFolders) {
+    vscode.window.showErrorMessage("No Workspace is open!");
+    return;
+  }
+  const workspacePath = workspaceFolders[0].uri.fsPath;
+  const folderPath = path.join(workspacePath, "schema");
+
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath, { recursive: true });
+    vscode.window.showInformationMessage(`Folder 'schema' created.`);
+  }
+  const content =
+    "-- SQL Database Preset One Schema\nCREATE TABLE example (id INT, data VARCHAR(100));";
+  const filepath = path.join(folderPath, "schema.sql");
+  fs.writeFile(filepath, content, (err:any) => {
+	if (err) {
+		vscode.window.showErrorMessage('Error writing file: ' + err.message);
+	} else {
+		vscode.window.showInformationMessage(`File schema.sql created at ${folderPath}`);
+	}
+  });
+
+  
+  fs.writeFileSync(filepath, content);
+  vscode.window.showInformationMessage("Blogging Schema Created");
 }
 
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "datai" is now active!');
 
-	console.log('Congratulations, your extension "datai" is now active!');
+  let disposable = vscode.commands.registerCommand("datai.helloWorld", () => {
+    vscode.window.showInformationMessage("Hello World from datai!");
+  });
 
-	let disposable = vscode.commands.registerCommand('datai.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from datai!');
-	});
-
-	//Test
-	let bloggingSchema = vscode.commands.registerCommand('datai.bloggingApplicationSchema', generateSchemaBlogging);
-	context.subscriptions.push(bloggingSchema);
-	context.subscriptions.push(disposable);
+  //Test
+  let bloggingSchema = vscode.commands.registerCommand(
+    "datai.bloggingApplicationSchema",
+    generateSchemaBlogging
+  );
+  context.subscriptions.push(bloggingSchema);
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
