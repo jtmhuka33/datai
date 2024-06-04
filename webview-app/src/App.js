2296016d123d121d77d095fff8d0f67513b7759f
jtmhuka33 <38496930+jtmhuka33@users.noreply.github.com>
2024-06-04 13:44:05 +0200

    feat: basic ui now complete

--- webview-app/src/App.js
+++ webview-app/src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import TableCard from "./components/TableCard/TableCard";
-import { Stack, Button, Grid } from "@mui/material";
+import { Stack, Button, Grid, Box } from "@mui/material";
 
 function App() {
   return (
     <>
-      <Grid width="33%" container>
-        <Grid sm={6} xl={12} item>
+      <Grid  width="33%" container>
+        <Grid sx={{marginBottom: 3}} sm={6} xl={12} item>
           <Stack spacing={8} direction="row">
-            <h4>Custom Schema Generator</h4>
+            <h2>Custom Schema Generator</h2>
+            <Box sx={{ flexGrow: 1}}/>
             <Button color="success" variant="contained">
               ADD TABLE
             </Button>
@@ -16,6 +17,12 @@ function App() {
         </Grid>
         <Grid display="block" xl={12} item>
           <TableCard />
+        </Grid>
+        <Grid marginTop={3} xl={12} item>
+          <Stack direction="row">
+          <Box sx={{ flexGrow: 1}}/>
+          <Button type="submit" variant="contained" color="success">Generate Schema</Button>
+          </Stack>
         </Grid>
       </Grid>
     </>
