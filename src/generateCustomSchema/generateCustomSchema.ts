804e9c156d60dcba2de907ef1eb95b9abf9db5bd
jtmhuka33 <jtmhuka33@gmail.com>
2025-01-05 14:55:19 +0100

    minor: formatting changes

--- src/generateCustomSchema/generateCustomSchema.ts
+++ src/generateCustomSchema/generateCustomSchema.ts
@@ -7,19 +7,24 @@ export async function generateCustomSchema (application_context: string | null, 
        messages:[
         {
             role: "system",
-            content: "You are helpful coding assistant whose primary job is to code a typical postgresql schema using the provided tables and accounting for all tables and their relationships. Use markdown"
+            content: "You are helpful coding assistant whose primary job is to code a typical postgresql schema using the provided tables and accounting for all tables and their relationships"
         },
         {
             role: "user",
-            content: `Generate a PostgreSQL schema based on the following user specifications. The schema should include all necessary tables and fields as described, with proper SQL syntax for creating tables. 
-                    Ensure that primary keys and foreign keys are correctly defined based on the user inputs. Additionally, consider any provided context about the application type to optimize the schema's 
-                    relevance and functionality.
+            content: `Generate a PostgreSQL schema based on the following user specifications.
+                The schema should include all necessary tables and fields as described,
+                with proper SQL syntax for creating tables. 
+                Ensure that primary keys and foreign keys are correctly defined based on the user inputs.
+                Additionally, consider any provided context about the application type to optimize the schema's 
+                relevance and functionality.
 
             Application Context: ${application_context}
             User-Defined Tables and Columns:
             ${table_definitions}
             
-            Please format the SQL schema with proper data types, constraints, and relationships. Include comments in the SQL for clarity. Only reply with SQL and no additional explanations or text`
+            Please format the SQL schema with proper data types, constraints, and relationships.
+            Include comments in the SQL for clarity.
+            Only reply with SQL and no additional explanations or text`
             
 
         }
